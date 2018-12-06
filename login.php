0969d0d519396b0fcb75b40dd2cda3ff1f6b43ed
IHIutch <jbhutch01@gmail.com>
2018-12-05 23:09:50 -0500

    Few more tweaks and changes

--- login.php
+++ login.php
@@ -2,7 +2,10 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-6 offset-3">
+        <div class="col-6 offset-3 mt-5">
+            <h1>Log In</h1>
+            <p>Log in to your account to see the your submitted 311 requests.</p>
+
             <form method="post" action="verify.php" name="login" id="login" autocomplete="off">
                 <div class="form-group">
                     <label>Email address</label>
--- profile.php
+++ profile.php
@@ -1,82 +1,81 @@
 <?php 
-session_start(); 
+include "inc/header.php";
+
 if(!$_SESSION['logged']){ 
     header("Location: login.php"); 
     exit; 
-} 
-
-include "inc/header.php";
+};
 
 $email = $_SESSION['email']; 
-?> 
+?>
 
 <div class="container">
     <div class="row">
-        <div class="col-12">
-            <p>Welcome <?php echo $email; ?></p>
+        <div class="col-12 mt-5 mb-3">
+            <h1>Welcome
+                <?php echo $email; ?>
+            </h1>
         </div>
     </div>
     <div class="row">
-                <div class="mb-3 col-12">
-                    <div class="p-4 bg-white rounded shadow-sm">
-                        <table id="datatable" class="table table-striped table-sm mb-0">
-                            <thead>
-                                <tr>
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Submit Date</th>
-                                    <th scope="col">Lat</th>
-                                    <th scope="col">Lng</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Type</th>
-                                    <th scope="col">Subtype</th>
-                                    <th scope="col"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php $data = showUserData($email); 
+        <div class="mb-3 col-12">
+            <div class="p-4 bg-white rounded shadow-sm">
+                <table id="datatable" class="table table-striped table-sm mb-0">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Submit Date</th>
+                            <th scope="col">Lat</th>
+                            <th scope="col">Lng</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Subtype</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $data = showUserData($email); 
                                     foreach($data as $d){ ?>
-                                <tr>
-                                    <th scope="row">
-                                        <?php echo $d['id']?>
-                                    </th>
-                                    <td>
-                                        <?php echo date('m.d.y', strtotime($d['submission_date']))?>
-                                    </td>
-                                    <td>
-                                        <?php echo $d['lat']?>
-                                    </td>
-                                    <td>
-                                        <?php echo $d['lng']?>
-                                    </td>
-                                    <td>
-                                        <?php echo $d['email']?>
-                                    </td>
-                                    <td>
-                                        <?php echo $d['type']?>
-                                    </td>
-                                    <td>
-                                        <?php echo $d['subtype']?>
-                                    </td>
-                                    <td><a href="report.php?report_id=<?php echo $d['id']?>">Link</a></td>
-                                </tr>
-                                <?php } ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                        <tr>
+                            <th scope="row">
+                                <?php echo $d['id']?>
+                            </th>
+                            <td>
+                                <?php echo date('m.d.y', strtotime($d['submission_date']))?>
+                            </td>
+                            <td>
+                                <?php echo $d['lat']?>
+                            </td>
+                            <td>
+                                <?php echo $d['lng']?>
+                            </td>
+                            <td>
+                                <?php echo $d['email']?>
+                            </td>
+                            <td>
+                                <?php echo $d['type']?>
+                            </td>
+                            <td>
+                                <?php echo $d['subtype']?>
+                            </td>
+                            <td><a href="report.php?report_id=<?php echo $d['id']?>">Link</a></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
             </div>
+        </div>
+    </div>
 </div>
 
-<a href="logout.php">Logout</a>
-
-
 <script>
-$(document).ready( function () {
-    $('#datatable').DataTable({
-        select: true,
-        responsive: true
+    $(document).ready(function() {
+        $('#datatable').DataTable({
+            select: true,
+            responsive: true
+        });
     });
-} );
+
 </script>
 
 <?php include "inc/footer.php";
