9661c85dec8563e9918e47abd3fa93dd33d4181e
IHIutch <jbhutch01@gmail.com>
2019-01-16 08:50:35 -0500

    Mapbox report fix

--- report.php
+++ report.php
@@ -256,8 +256,7 @@ if(isset($_POST['status'])){
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
-        accessToken: '<?php echo env('
-        MAPBOX '); ?>'
+        accessToken: '<?php echo env('MAPBOX'); ?>'
     }).addTo(map);
 
     var point = <?php echo json_encode($pointData['features']); ?>;
