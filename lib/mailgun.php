8b3c23158cc7d48efd710ca85a0f4e8175e7013e
IHIutch <jbhutch01@gmail.com>
2019-03-01 21:29:09 -0500

    Double to single quotes, make MG key a var

--- lib/mailgun.php
+++ lib/mailgun.php
@@ -2,21 +2,21 @@
 
 use Mailgun\Mailgun;
 
-define('MAILGUN', getenv('MAILGUN'));
+$mailgun = getenv('MAILGUN'));
 
 function mail_submit($to, $details){
     
-    $mg = Mailgun::create(MAILGUN); // For US servers
+    $mg = Mailgun::create($mailgun); // For US servers
     
     $html = file_get_contents(__DIR__.'/../templates/emails/submit-ticket.html');
-    $html = str_replace("#id#",$details['id'],$html);
-    $html = str_replace("#type#",$details['type'],$html);
-    $html = str_replace("#subtype#",$details['subtype'],$html);
-    $html = str_replace("#street_num#",$details['street_num'],$html);
-    $html = str_replace("#street_name#",$details['street_name'],$html);
-    $html = str_replace("#zip#",$details['postal_code'],$html);   
+    $html = str_replace('#id#',$details['id'],$html);
+    $html = str_replace('#type#',$details['type'],$html);
+    $html = str_replace('#subtype#',$details['subtype'],$html);
+    $html = str_replace('#street_num#',$details['street_num'],$html);
+    $html = str_replace('#street_name#',$details['street_name'],$html);
+    $html = str_replace('#zip#',$details['postal_code'],$html);   
     $link = 'https://'.$_SERVER['SERVER_NAME'].'/report/'.$details['id'];
-    $html = str_replace("#link#",$link,$html);
+    $html = str_replace('#link#',$link,$html);
     
     $mg->messages()->send('beta.buffalo311.org', [
       'from'       => 'Buffalo 311 <info@beta.buffalo311.org>',
