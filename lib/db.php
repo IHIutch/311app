e71085abcffa01b45fdaea7ff6e45aa883d0eff5
IHIutch <jbhutch01@gmail.com>
2019-03-01 21:03:52 -0500

    db.php

---
+++ lib/db.php
@@ -0,0 +1,10 @@
+<?php
+define('DBHOST', getenv('DBHOST'));
+define('DBUSER', getenv('DBUSER'));
+define('DBPASS', getenv('DBPASS'));
+define('DBNAME', getenv('DBNAME'));
+
+require __DIR__ .'/vendor/j4mie/idiorm/idiorm.php';
+ORM::configure('mysql:host='.DBHOST.';dbname='.DBNAME);
+ORM::configure('username', DBUSER);
+ORM::configure('password', DBPASS);
